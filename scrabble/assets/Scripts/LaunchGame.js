730ef15ccc3c9bc3331141cb1e97c861334a280c
Mioduu <macieos360x@gmail.com>
2025-10-12 18:39:53 +0200

    Small changes

--- scrabble/assets/Scripts/LaunchGame.js
+++ scrabble/assets/Scripts/LaunchGame.js
@@ -1,17 +1,29 @@
 const startBtn = document.getElementById("startBtn")
 const settingsBtn = document.getElementById("settingsBtn")
 const title = document.getElementById("title")
-const serversDiv = document.getElementById("serversDiv")
+const localBtn = document.getElementById("localBtn")
+const onlineBtn = document.getElementById("onlineBtn")
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    startBtn.style.display = "flex"
-    settingsBtn.style.display = "flex"
+    localBtn.style.display = "none"
+    onlineBtn.style.display = "none"
 })
 
 export function startGame() {
     console.log("Button clicked")
-    startBtn.style.display = "none"
-    settingsBtn.style.display = "none"
-    title.textContent = "Servers:"
-    serversDiv.style.display = "flex"
+    startBtn.classList.add("fade-out")
+    settingsBtn.classList.add("fade-out")
+
+    setTimeout(() => {
+        startBtn.style.display = "none"
+        settingsBtn.style.display = "none"
+        title.textContent = "Servers:"
+        title.classList.add("fade-in")
+        localBtn.style.display = "flex"
+        onlineBtn.style.display = "flex"
+        localBtn.classList.add("fade-in")
+        onlineBtn.classList.add("fade-in")
+    }, 300);
+    
 }
--- scrabble/src/renderer.js
+++ scrabble/src/renderer.js
@@ -1,6 +1,5 @@
 import {startGame} from '../assets/Scripts/LaunchGame.js';
 
-const startBtn = document.getElementById("startBtn")
-
 startBtn.addEventListener("click", startGame)
+
 
